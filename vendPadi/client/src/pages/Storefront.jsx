1075799bdf7ef93ff031d8c061618da43d2b23c6
eemanDigital <eemandigitalconcept@gamil.com>
2026-05-09 21:10:29 +0100

    fix: add Link import to Storefront for navigation enhancements

--- vendPadi/client/src/pages/Storefront.jsx
+++ vendPadi/client/src/pages/Storefront.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useMemo } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { initWishlist } from "../store/wishlistSlice";
 import { clearCart } from "../store/cartSlice";
